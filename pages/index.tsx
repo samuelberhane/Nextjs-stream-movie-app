6cfbb438909f171148e87d7f4ae6a52dfcf2dc09
Samuel Brhane <61617535+samuelberhane@users.noreply.github.com>
2023-01-02 10:16:22 +0300

    install nextjs and tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Home - Stream Netflix Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1>Stream Netflix Clone</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
